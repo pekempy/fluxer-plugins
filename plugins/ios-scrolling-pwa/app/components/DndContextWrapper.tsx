7306acba5ba8881211cfd682f26f0ed92ffab4e5
Pekempy <pete@pekempy.co.uk>
2026-07-09 23:14:58 +0100

    feat: register react-dnd-touch-backend to enable touch drag-and-drop on mobile layout

---
+++ plugins/ios-scrolling-pwa/app/components/DndContextWrapper.tsx
@@ -0,0 +1,49 @@
+// @ts-nocheck
+import React from 'react';
+import {DndProvider} from 'react-dnd';
+import KeyboardBackend, {isKeyboardDragTrigger} from 'react-dnd-accessible-backend';
+import {HTML5Backend} from 'react-dnd-html5-backend';
+import {createTransition, MouseTransition, MultiBackend, TouchTransition} from 'react-dnd-multi-backend';
+import {TouchBackend} from 'react-dnd-touch-backend';
+
+const KeyboardTransition = createTransition('keydown', (event: Event) => {
+	if (!isKeyboardDragTrigger(event as KeyboardEvent)) return false;
+	event.preventDefault();
+	return true;
+});
+
+const TOUCH_DND_OPTIONS = {
+	backends: [
+		{
+			id: 'html5',
+			backend: HTML5Backend,
+			transition: MouseTransition,
+		},
+		{
+			id: 'touch',
+			backend: TouchBackend,
+			options: { enableMouseEvents: false },
+			preview: true,
+			transition: TouchTransition,
+		},
+		{
+			id: 'keyboard',
+			backend: KeyboardBackend,
+			context: {window, document},
+			preview: true,
+			transition: KeyboardTransition,
+		},
+	],
+};
+
+const DndContextWrapper = (OriginalComponent: any) => {
+  return (props: any) => {
+    return (
+      <DndProvider backend={MultiBackend} options={TOUCH_DND_OPTIONS}>
+        {props.children}
+      </DndProvider>
+    );
+  };
+};
+
+export default DndContextWrapper;
